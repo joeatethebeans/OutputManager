53e60463fb8e6325e5a6504cfe8cb9ddf354b0b5
joeatethebeans <joewood.hav@gmail.com>
2026-09-05 11:50:06 +0100

    chore: add version number to built zip

--- build-zip.sh
+++ build-zip.sh
@@ -8,7 +8,8 @@ echo "Building frontend..."
 npm run build
 
 PLUGIN_NAME=$(node -p "require('./plugin.json').name")
-echo "Plugin name: $PLUGIN_NAME"
+VERSION=$(node -p "require('./package.json').version")
+echo "Plugin name: $PLUGIN_NAME (v$VERSION)"
 
 STAGE_DIR="$(mktemp -d)"
 TARGET_DIR="$STAGE_DIR/$PLUGIN_NAME"
@@ -17,7 +18,7 @@ mkdir -p "$TARGET_DIR"
 cp plugin.json main.py package.json "$TARGET_DIR/"
 cp -r dist "$TARGET_DIR/"
 
-OUTPUT_ZIP="$PROJECT_DIR/${PLUGIN_NAME}.zip"
+OUTPUT_ZIP="$PROJECT_DIR/${PLUGIN_NAME}-${VERSION}.zip"
 rm -f "$OUTPUT_ZIP"
 
 (
